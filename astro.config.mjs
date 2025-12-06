3f2a38111a36f46e25e8ca7faff4918db845e04f
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2025-12-06 14:52:54 +0100

    site et base dans astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://worldwise-admission.com/",
+  site: 'https://worldwise-admission.com',
+  base: '/',
   integrations: [
     tailwind(),
     icon(),
